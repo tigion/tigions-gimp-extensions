27013591621ec08efe34b5cbb14d1db0c56f97b4
Christoph Zirkelbach <tigion@bsd-crew.de>
2008-11-09 09:56:02 +0100

    New functionality: option for a more realistic shadow (a little bit tricky)

--- script-fu-photo-double-border-te.scm
+++ script-fu-photo-double-border-te.scm
@@ -1,16 +1,30 @@
 ; TODO:
-; [ ] Lizenshinweis
+; - Lizenshinweis
+; - Anwendung auf Größe des aktuellen Layers oder auf gesamtes Bild
 
 ; define function
-(define (script-fu-photo-double-border-te img drawable border_color border_size passepartout_color passepartout_size use_merge_layers)
+(define 
+  (script-fu-photo-double-border-te
+    img
+    drawable
+    border_color
+    border_size
+    passepartout_color
+    passepartout_size
+    use_real_shadows
+    use_merge_layers
+  )
+
   (let*
     (
       (save_color_foreground (car (gimp-context-get-foreground)))
+      (save_color_background (car (gimp-context-get-background)))
       (img_width (car (gimp-image-width img)))
       (img_height (car (gimp-image-height img)))
 
       (use_passepartout FALSE)
       (use_border FALSE)
+      (use_border_outside_shadow FALSE)
 
       (new_img_width)
       (new_img_height)
@@ -23,8 +37,16 @@
 
       (newLayer_border)
       (newLayer_border_Shadow)
+      (newLayer_border_Shadow_inside)
+      (newLayer_border_Shadow_outside)
       (newLayer_passepartout)
       (newLayer_passepartout_Shadow)
+
+      (new_mask)
+
+      (color_black '(0 0 0))
+      (color_white '(255 255 255))
+      (shadow_color color_black)
     )
 
     ; ---------------------------------------------------------------
@@ -34,6 +56,9 @@
     ; set borders to draw
     (if (> passepartout_size 0) (set! use_passepartout TRUE))
     (if (> border_size 0) (set! use_border TRUE))
+
+    ; set outside shadow
+    (if (= use_real_shadows TRUE) (set! use_border_outside_shadow TRUE))
 
     ; calc new size
     (set! new_img_width  (+ img_width  (* 2 border_size) (* 2 passepartout_size)))
@@ -129,7 +154,21 @@
 
         ; draw drop shadow
         ; TODO: temporary hard coded
-        (script-fu-drop-shadow img newLayer_passepartout 0 0 5 '(0 0 0) 20 FALSE)
+        (script-fu-drop-shadow img newLayer_passepartout 0 0 5 shadow_color 20 FALSE)
+        ; notice and rename new shadow layer
+        ; TODO: - a little bit tricky?
+        ;       - exclude in an extra function
+        (set! newLayer_passepartout_Shadow 
+          (aref 
+            (cadr (gimp-image-get-layers img))
+            (+ (car (gimp-image-get-layer-position img newLayer_passepartout)) 1)
+          )
+        )
+        (gimp-drawable-set-name newLayer_passepartout_Shadow "Passepartout Shadow")
+
+        ; resize shadow layer to image size
+        (gimp-layer-resize-to-image-size newLayer_passepartout_Shadow)
+
       )
     )
 
@@ -180,21 +219,149 @@
         ; clear selection
         (gimp-selection-none img)
 
-        ; draw drop shadow
-        ; TODO: temporary hard coded
-        (script-fu-drop-shadow img newLayer_border 0 0 10 '(0 0 0) 80 FALSE)
+        ; draw shadows
+        (if (= use_real_shadows FALSE)
+          ; draw drop shadow - simple
+          (begin
+            ; draw shadow inside and outside
+            ; TODO: temporary hard coded
+            (script-fu-drop-shadow img newLayer_border 0 0 10 shadow_color 80 FALSE)
+            ; notice and rename new shadow layer
+            ; TODO: - a little bit tricky?
+            ;       - exclude in an extra function
+            (set! newLayer_border_Shadow 
+              (aref 
+                (cadr (gimp-image-get-layers img))
+                (+ (car (gimp-image-get-layer-position img newLayer_border)) 1)
+              )
+            )
+            (gimp-drawable-set-name newLayer_border_Shadow "Border Shadow")
+          )
+
+          ; draw drop shadow - realistic
+          (begin
+            ; ----------------------------
+            ; draw shadow inside
+            ; ----------------------------
+
+            ; TODO: temporary hard coded
+            (script-fu-drop-shadow img newLayer_border 0 2 10 shadow_color 80 FALSE)
+
+            ; notice and rename new shadow layer
+            ; TODO: - a little bit tricky?
+            ;       - exclude in an extra function
+            (set! newLayer_border_Shadow_inside 
+              (aref 
+                (cadr (gimp-image-get-layers img))
+                (+ (car (gimp-image-get-layer-position img newLayer_border)) 1)
+              )
+            )
+            (gimp-drawable-set-name newLayer_border_Shadow_inside "Border Shadow (inside)")
+            
+            ; create mask
+            (set! new_mask (car (gimp-layer-create-mask newLayer_border_Shadow_inside ADD-BLACK-MASK)))
+            (gimp-layer-add-mask newLayer_border_Shadow_inside new_mask)
+            (gimp-layer-get-edit-mask newLayer_border_Shadow_inside)
+            ;(gimp-image-set-active-layer img newLayer_border_Shadow_inside)
+
+            ; create rectangle selection
+            (gimp-rect-select
+              img
+              (- xPos passepartout_size)
+              (- yPos passepartout_size)
+              select_width1
+              select_height1
+              0 ;operation
+              0 ;feather
+              0 ;feather-radius
+            )
+
+            ; fill selection
+            (gimp-context-set-background color_white)
+            (gimp-edit-fill new_mask BACKGROUND-FILL)
+
+            ; ...
+            ;(gimp-image-set-active-layer img newLayer_border)
+
+            ; ----------------------------
+            ; draw shadow outside
+            ; ----------------------------
+
+            ; clear possible selections
+            (gimp-selection-none img)
+
+            ;
+            (gimp-image-set-active-layer img newLayer_border)
+
+            ; TODO: temporary hard coded
+            (script-fu-drop-shadow img newLayer_border 0 4 15 shadow_color 80 FALSE)
+
+            ; notice and rename new shadow layer
+            ; TODO: - a little bit tricky?
+            ;       - exclude in an extra function
+            (set! newLayer_border_Shadow_outside 
+              (aref 
+                (cadr (gimp-image-get-layers img))
+                (+ (car (gimp-image-get-layer-position img newLayer_border)) 1)
+              )
+            )
+            (gimp-drawable-set-name newLayer_border_Shadow_outside "Border Shadow (outside)")
+ 
+            ; create mask
+            (set! new_mask (car (gimp-layer-create-mask newLayer_border_Shadow_outside ADD-WHITE-MASK)))
+            (gimp-layer-add-mask newLayer_border_Shadow_outside new_mask)
+            (gimp-layer-get-edit-mask newLayer_border_Shadow_outside)
+            ;(gimp-image-set-active-layer img newLayer_border_Shadow_outside)
+
+            ; create rectangle selection
+            (gimp-rect-select
+              img
+              0 ;(- xPos passepartout_size)
+              0 ;(- yPos passepartout_size)
+              new_img_width
+              new_img_height
+              0 ;operation
+              0 ;feather
+              0 ;feather-radius
+            )
+
+            ; fill selection
+            (gimp-context-set-background color_black)
+            (gimp-edit-fill new_mask BACKGROUND-FILL)
+          )
+        )
       )
     )
 
     ; ---------------------------------------------------------------
-    ; merge layers
+    ; finish result (options)
     ; ---------------------------------------------------------------
-    
+
+    ; resize image to show outside shadow
+    (if (= use_border_outside_shadow TRUE)
+      (gimp-image-resize-to-layers img)
+      ;(gimp-layer-resize-to-image-size newLayer_border)
+    )
+
+    ; merge result layers 
     (if (= use_merge_layers TRUE)
       (begin
-        (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
-        (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
-        (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
+        (if (= use_real_shadows FALSE)
+          ; merge simple version
+          (begin
+            (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
+            (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
+            (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
+          )
+          ; merge realistic version
+          (begin
+            (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
+            (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
+            (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
+            (set! newLayer_border (car (gimp-image-merge-down img newLayer_border EXPAND-AS-NECESSARY)))
+          )
+        )
+        ; rename result layer
         (gimp-drawable-set-name newLayer_border "Border+Passepartout")
       )
     )
@@ -208,6 +375,7 @@
 
     ; restore colors
     (gimp-context-set-foreground save_color_foreground)
+    (gimp-context-set-background save_color_background)
 
     ; ---------------------------------------------------------------
     ; end point for undo group
@@ -225,7 +393,7 @@
     ; return values
     (if (= use_merge_layers TRUE)
       (list newLayer_border)
-      (list newLayer_border newLayer_border_Shadow newLayer_passepartout newLayer_passepartout_Shadow)
+      ;(list newLayer_border newLayer_border_Shadow newLayer_passepartout newLayer_passepartout_Shadow)
     )
   )
 )
@@ -237,7 +405,7 @@
   "Creates a double border around a photo." ; description
   "Christoph Zirkelbach" ; author
   "Christoph Zirkelbach" ; copyright notice
-  "November 5, 2008" ; date created
+  "November 9, 2008" ; date created
   "" ; image type that the script works on
   SF-IMAGE       "Image" 0
   SF-DRAWABLE    "Drawable" 0
@@ -245,7 +413,7 @@
   SF-ADJUSTMENT  "Breite des Rahmens" '(10 1 1000 1 10 0 1)
   SF-COLOR       "Passepartoutfarbe (innen)" '(255 255 255)
   SF-ADJUSTMENT  "Breite des Passepartouts" '(20 0 1000 1 10 0 1)
+  SF-TOGGLE      "Realistischere Schatten darstellen (auch ausserhalb)" FALSE
   SF-TOGGLE      "Ergebnislayer zusammenfassen" TRUE
 )
 (script-fu-menu-register "script-fu-photo-double-border-te" "<Image>/Script-Fu/Photo/")
-
